3fb8e5c3e8c12aadcc3c19db34af8efb0ec24987
seudad <romanzaytsev2015@gmail.com>
2024-03-14 01:46:59 +0300

    some

--- profclimatorg/settings.py
+++ profclimatorg/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-from django.conf.global_settings import INTERNAL_IPS, MEDIA_ROOT, MEDIA_URL, STATICFILES_DIRS
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
