d063e296ecc91d5923bf2030a66d78767c2b9c0c
seudad <romanzaytsev2015@gmail.com>
2024-04-05 16:49:09 +0300

    set allowed hosts in settings

--- profclimatorg/settings.py
+++ profclimatorg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-&@rrm9&*-t@9frj^6v7&@gw0*y$&fgedw!#rfixwzl@7^ymfq9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['test.profclimat.org']
 
 
 # Application definition
